7e1088bbda20b06fba8080476a03dacf37db3ac1
dureamPark <doo7172@naver.com>
2025-02-02 17:17:02 +0900

    BasicMonster Hit motion add and Server matching test1

---
+++ NecroSyntex/Source/NecroSyntex/Monster/AC_Monster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AC_Monster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NECROSYNTEX_API AAC_Monster : public AAIController
+{
+	GENERATED_BODY()
+	
+};
