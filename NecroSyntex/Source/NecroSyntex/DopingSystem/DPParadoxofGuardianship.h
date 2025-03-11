59ea474a7893fa5b23d8d84c5ccfb0d36a599208
PapaHu22 <pth000422@gmail.com>
2025-03-12 03:48:47 +0900

    .0312temp

---
+++ NecroSyntex/Source/NecroSyntex/DopingSystem/DPParadoxofGuardianship.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DopingParent.h"
+#include "DPParadoxofGuardianship.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NECROSYNTEX_API UDPParadoxofGuardianship : public UDopingParent
+{
+	GENERATED_BODY()
+	
+};
