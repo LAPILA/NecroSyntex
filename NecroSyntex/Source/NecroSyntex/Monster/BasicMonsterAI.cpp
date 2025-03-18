7a9d9b8f78574e06f4604426544dd15454b72c20
dureamPark <doo7172@naver.com>
2025-03-18 13:57:38 +0900

    Revert "Merge branch 'TempCommit' into WMDP_Save"
    This reverts commit b337e3fe8723a9f19501429ec0a71945fbb2550c, reversing
    changes made to e43ddd1cb6d5dddac3020118747863525aea5058.

--- NecroSyntex/Source/NecroSyntex/Monster/BasicMonsterAI.cpp
+++ NecroSyntex/Source/NecroSyntex/Monster/BasicMonsterAI.cpp
@@ -2,7 +2,6 @@
 
 
 #include "BasicMonsterAI.h"
-#include "TimerManager.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
@@ -11,7 +10,6 @@ ABasicMonsterAI::ABasicMonsterAI()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	MonsterHP = 100.0f;
 }
 
 // Called when the game starts or when spawned
@@ -40,63 +38,4 @@ void ABasicMonsterAI::UpdateWalkSpeed(float NewWalkSpeed) {
 	{
 		GetCharacterMovement()->MaxWalkSpeed = NewWalkSpeed;
 	}
-}
-
-float ABasicMonsterAI::TakeDamage_Implementation(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
-{
-	if (MonsterHP <= 0.0f) {
-		return 0.0f; // 이미 죽었으면 데미지 무효
-	}
-
-	MonsterHP -= DamageAmount; // 체력 감소
-
-	PlayHitAnimation();
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Hit!!!"));
-	}
-
-	//UE_LOG(LogTemp, Warning, TEXT("Monster Hit! Remaining Health: %f"), MonsterHP);
-
-	// 체력이 0 이하이면 몬스터 제거
-	if (MonsterHP <= 0.0f)
-	{
-		PlayDeathAnimation();
-		UE_LOG(LogTemp, Warning, TEXT("Monster is Dead!"));
-		DelayedFunction(3.0f);
-		//Destroy();
-	}
-
-	return DamageAmount;
-}
-
-void ABasicMonsterAI::PlayHitAnimation()
-{
-	// 애니메이션이 설정되어 있는 경우 실행
-	if (HitReactionMontage && GetMesh() && GetMesh()->GetAnimInstance())
-	{
-		GetMesh()->GetAnimInstance()->Montage_Play(HitReactionMontage);
-	}
-}
-
-void ABasicMonsterAI::PlayDeathAnimation()
-{
-	// 애니메이션이 설정되어 있는 경우 실행
-	if (DeathReactionMontage && GetMesh() && GetMesh()->GetAnimInstance())
-	{
-		GetMesh()->GetAnimInstance()->Montage_Play(DeathReactionMontage);
-	}
-}
-
-void ABasicMonsterAI::DelayedFunction(float DelayTime)
-{
-	FTimerHandle handle;
-	GetWorld()->GetTimerManager().SetTimer(handle, this, &ABasicMonsterAI::DestroyMonster, DelayTime, false);
-	//GetWorld()->GetTimerManager().SetTimer(handle, FTimerDelegate::CreateLambda([this]() { PlayDeathAnimation();}), DelayTime, false);
-}
-
-void ABasicMonsterAI::DestroyMonster()
-{
-	Destroy(); // AActor의 Destroy() 실행
 }
--- NecroSyntex/Source/NecroSyntex/Monster/BasicMonsterAI.h
+++ NecroSyntex/Source/NecroSyntex/Monster/BasicMonsterAI.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Animation/AnimMontage.h"
 #include "BasicMonsterAI.generated.h"
 
 UCLASS()
@@ -23,36 +22,9 @@ public:
 	UPROPERTY(EditAnywhere, Category = "AI")
 	float ChaseSpeed = 600.0f;
 
-	UFUNCTION(BlueprintNativeEvent, Category = "Damage")
-	float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
-	virtual float TakeDamage_Implementation(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
-
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float MonsterHP;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
-	UAnimMontage* HitReactionMontage;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
-	UAnimMontage* DeathReactionMontage;
-
-	// 애니메이션 실행 함수
-	void PlayHitAnimation();
-
-	void PlayDeathAnimation();
-
-	void DestroyMonster();
-
-	//Death 이후 delaytime이후에 destroy
-	FTimerHandle DeathDelayTimerHandle;
-	//Timer Function
-	void DelayedFunction(float DelayTime);
-
 
 public:	
 	// Called every frame
@@ -60,4 +32,5 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 };
