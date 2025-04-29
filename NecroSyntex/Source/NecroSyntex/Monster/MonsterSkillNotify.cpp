2fa786dbd581bebcf097d3c4f845b07b9579aee2
dureamPark <doo7172@naver.com>
2025-04-29 12:23:59 +0900

    Revert "Monster Sound Asset Add and Monster Chase Update Not Complete"
    This reverts commit cbf0d8324c5df7ddf56eb1766434bd24b503a885.

--- NecroSyntex/Source/NecroSyntex/Monster/MonsterSkillNotify.cpp
+++ NecroSyntex/Source/NecroSyntex/Monster/MonsterSkillNotify.cpp
@@ -8,12 +8,12 @@
 void UMonsterSkillNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
 {
 	ABasicMonsterAI* Monster = Cast<ABasicMonsterAI>(MeshComp->GetOwner());
-	//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Notify!!!"));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Notify!!!"));
 
 	for (AActor* Target : Monster->GetOverlappingPlayers()) {
 		float DamageAmount = Monster->MonsterAD * 2; // ¶Ç´Â MonsterAD
 		UGameplayStatics::ApplyDamage(Target, DamageAmount, Monster->GetController(), Monster, nullptr); //targetactor, targetdamage, causercontroller, causeractor, damagetype
-		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Damage!!!!!!!!!!"));
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Damage!!!!!!!!!!"));
 	}
 }
 
