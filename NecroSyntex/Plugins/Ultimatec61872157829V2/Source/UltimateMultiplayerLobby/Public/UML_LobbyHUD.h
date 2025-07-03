e995dd178899bba199270dc2ac8eedd056d3f8e5
dureamPark <doo7172@naver.com>
2025-07-04 01:39:44 +0900

    Lobby asset recover.
    아직 lobby asset에서 초대기능을 연결해둔게 아닙니다.

---
+++ NecroSyntex/Plugins/Ultimatec61872157829V2/Source/UltimateMultiplayerLobby/Public/UML_LobbyHUD.h
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2024 Betide Studio. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/HUD.h"
+#include "TimerManager.h"
+#include "UML_LobbyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULTIMATEMULTIPLAYERLOBBY_API AUML_LobbyHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Ultimate Game Menu|Lobby HUD")
+	bool OnLoggedIn();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Ultimate Game Menu|Lobby HUD")
+	bool OnLoggedOut();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Ultimate Game Menu|Lobby HUD")
+	bool OnLoginFailed();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Ultimate Game Menu|Lobby HUD")
+	bool OnLoginProcessStarted();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Ultimate Game Menu|Lobby HUD")
+	bool OnManualLoginRequired();
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ultimate Game Menu|Lobby HUD")
+	TSubclassOf<UUserWidget> LoginInProgressWidgetClass;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Ultimate Game Menu|Lobby HUD")
+	TSubclassOf<UUserWidget> LobbyMenuWidgetClass;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ultimate Game Menu|Lobby HUD")
+	UUserWidget* LoginInProgressWidget;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Ultimate Game Menu|Lobby HUD")
+	UUserWidget* LobbyMenuWidget;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Ultimate Game Menu|Lobby HUD")
+	bool bAutoLogin = true;
+	
+
+protected:
+	FTimerHandle LoginStatusCheckTimerHandle;
+	void CheckLoginStatus();
+	virtual void BeginPlay() override;
+};
