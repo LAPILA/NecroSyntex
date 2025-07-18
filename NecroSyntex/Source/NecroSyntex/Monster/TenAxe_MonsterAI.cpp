9eec9ea15d55dcb4ee183b8da5f36618c1b4b2ab
dureamPark <doo7172@naver.com>
2025-07-19 01:04:54 +0900

    Make elite monster can basic attack move.

---
+++ NecroSyntex/Source/NecroSyntex/Monster/TenAxe_MonsterAI.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TenAxe_MonsterAI.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
+
+ATenAxe_MonsterAI::ATenAxe_MonsterAI()
+{
+
+}
+
+void ATenAxe_MonsterAI::UpdateMaxWalkSpeed(float inputSpeed)
+{
+	if (GetCharacterMovement()) {
+		GetCharacterMovement()->MaxWalkSpeed = inputSpeed;
+	}
+}
+
+
